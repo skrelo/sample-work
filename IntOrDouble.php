4c4b24957e6775acdf9c4d40fefed86632b0b52f
Scott Krelo <skrelo@gmail.com>
2016-05-31 08:35:19 -0600

    Update IntOrDouble.php

--- IntOrDouble.php
+++ IntOrDouble.php
@@ -1,6 +1,6 @@
 <?php
 if ( isset ( $argv[1] ) && !empty( $argv[1]) ) {
-        $string = $argv[1] + 0;
+        $string = $argv[1];
         if( is_numeric( $string ) ) {
                 if( is_float( $string ) ) {
                         echo "String is Double\n";
@@ -8,7 +8,15 @@ if ( isset ( $argv[1] ) && !empty( $argv[1]) ) {
                 }elseif( is_int( $string) ) {
                         echo "String is Int\n";
                         die();
+                }else {
+                        echo "Number is of unknown type\n";
+                        die();
                 }
-        } 
+        }else {
+                echo "String is not Int or Double\n";
+                die();
+        }
+}else {
+        echo "You must provide a input variable\n";
+        die();
 }
-echo "String is not Int or Double\n";
